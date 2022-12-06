1da85597d047696121a23d5adf6e52aa1f6238f8
Elena2602 <118214409+Elena2602@users.noreply.github.com>
2022-12-06 14:36:16 +0300

    domTrast

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Task 10
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число:  "); 
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = n % 100; 
+int n3 = n1 / 10; 
+
+    Console.WriteLine(n3);
